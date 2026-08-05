bb525f169b83961829a834ecc706996a6ba7ba90
Rospopa <Pavlo.Rospopa@marcusmillichap.com>
2026-08-05 10:44:13 -0500

    ui: rounded morphing clouds with gradient + opacity animation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -79,26 +79,45 @@ export default function App() {
   return (
     <div className="app-root">
       <div className="dynamic-bg" aria-hidden="true">
-        <svg className="bg-clouds" viewBox="0 0 1200 140" preserveAspectRatio="xMidYMin slice" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-          <path fill="#ffffff" opacity="0.9" d="M50 60c20-20 80-20 120 0s60 40 120 20 140-40 220 0 140 40 200 20 100-60 160-40v40H50z">
-            <animate attributeName="d" dur="14s" repeatCount="indefinite" values="M50 60c20-20 80-20 120 0s60 40 120 20 140-40 220 0 140 40 200 20 100-60 160-40v40H50z;
-                                                                                             M30 70c30-30 90-10 140 10s40 30 110 10 160-30 200 10 120 40 180 0 120-80 180-40v40H30z;
-                                                                                             M60 50c10-10 70-30 130-10s70 50 140 30 120-30 200 0 160 60 220 20 100-60 160-40v40H60z;
-                                                                                             M50 60c20-20 80-20 120 0s60 40 120 20 140-40 220 0 140 40 200 20 100-60 160-40v40H50z" />
+        <svg className="bg-clouds" viewBox="0 0 1200 160" preserveAspectRatio="xMidYMin slice" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+          <defs>
+            <linearGradient id="cloudGrad1" x1="0" x2="1" y1="0" y2="0">
+              <stop offset="0%" stopColor="#fff" stopOpacity="0.98" />
+              <stop offset="100%" stopColor="#fff" stopOpacity="0.85" />
+            </linearGradient>
+            <linearGradient id="cloudGrad2" x1="0" x2="1" y1="0" y2="0">
+              <stop offset="0%" stopColor="#fff" stopOpacity="0.9" />
+              <stop offset="100%" stopColor="#fff" stopOpacity="0.7" />
+            </linearGradient>
+            <filter id="cloudBlur" x="-20%" y="-20%" width="140%" height="140%">
+              <feGaussianBlur stdDeviation="8" result="b" />
+              <feMerge><feMergeNode in="b" /></feMerge>
+            </filter>
+          </defs>
+
+          <!-- three rounded overlapping cloud shapes with soft gradients and morph + opacity animation -->
+          <path fill="url(#cloudGrad1)" filter="url(#cloudBlur)" opacity="0.95" d="M40 80c40-40 120-40 180-10s90 60 170 40 160-60 260-20 160 40 220 10v50H40z">
+            <animate attributeName="d" dur="14s" repeatCount="indefinite" values="M40 80c40-40 120-40 180-10s90 60 170 40 160-60 260-20 160 40 220 10v50H40z;
+                                                                                             M20 90c50-20 110-10 190 10s70 40 150 20 180-50 260-10 120 20 200-10v50H20z;
+                                                                                             M60 70c30-50 140-30 200-6s100 48 180 28 140-48 240-8 180 34 240 6v50H60z;
+                                                                                             M40 80c40-40 120-40 180-10s90 60 170 40 160-60 260-20 160 40 220 10v50H40z" />
+            <animate attributeName="opacity" dur="14s" repeatCount="indefinite" values="0.92;0.6;0.85;0.92" />
           </path>
 
-          <path fill="#ffffff" opacity="0.85" d="M600 40c30-12 80-8 120 6s50 30 110 18 100-28 160-4 90 40 150 20v30H600z">
-            <animate attributeName="d" dur="18s" repeatCount="indefinite" values="M600 40c30-12 80-8 120 6s50 30 110 18 100-28 160-4 90 40 150 20v30H600z;
-                                                                                              M580 50c20-20 70-10 110 4s60 34 120 22 90-20 150 0 120 40 160 10v30H580z;
-                                                                                              M620 30c10-6 60-22 110-6s40 36 100 22 120-18 180 6 100 34 140 6v30H620z;
-                                                                                              M600 40c30-12 80-8 120 6s50 30 110 18 100-28 160-4 90 40 150 20v30H600z" />
+          <path fill="url(#cloudGrad2)" filter="url(#cloudBlur)" opacity="0.8" d="M520 50c40-20 100-10 150 10s60 38 120 20 140-36 200-8 120 30 180 12v40H520z">
+            <animate attributeName="d" dur="18s" repeatCount="indefinite" values="M520 50c40-20 100-10 150 10s60 38 120 20 140-36 200-8 120 30 180 12v40H520z;
+                                                                                             M500 60c30-30 120-6 170 14s80 30 140 18 160-24 220-2 100 28 160 8v40H500z;
+                                                                                             M540 40c20-10 90-26 160-4s50 42 110 26 120-20 180 4 140 26 200 6v40H540z;
+                                                                                             M520 50c40-20 100-10 150 10s60 38 120 20 140-36 200-8 120 30 180 12v40H520z" />
+            <animate attributeName="opacity" dur="18s" repeatCount="indefinite" values="0.85;0.55;0.75;0.85" />
           </path>
 
-          <path fill="#ffffff" opacity="0.8" d="M1000 80c-30-20-80-40-140-20s-80 50-140 40-120-20-180 0-120 10-180-10v30h740z">
-            <animate attributeName="d" dur="16s" repeatCount="indefinite" values="M1000 80c-30-20-80-40-140-20s-80 50-140 40-120-20-180 0-120 10-180-10v30h740z;
-                                                                                                 M980 70c-20-10-70-30-120-14s-70 44-130 34-140-16-200 6-100 30-160 10v30h820z;
-                                                                                                 M1020 90c-40-30-90-50-150-30s-90 60-150 50-100-10-160 10-140 14-200-6v30h840z;
-                                                                                                 M1000 80c-30-20-80-40-140-20s-80 50-140 40-120-20-180 0-120 10-180-10v30h740z" />
+          <path fill="#ffffff" filter="url(#cloudBlur)" opacity="0.78" d="M920 90c-50-30-120-50-200-28s-120 56-200 40-140-20-200 0-140 10-200-10v40h920z">
+            <animate attributeName="d" dur="16s" repeatCount="indefinite" values="M920 90c-50-30-120-50-200-28s-120 56-200 40-140-20-200 0-140 10-200-10v40h920z;
+                                                                                             M900 80c-30-10-100-36-170-16s-110 48-190 34-160-14-220 6-100 32-160 12v40h900z;
+                                                                                             M940 100c-60-40-130-60-210-36s-110 64-190 50-120-6-180 14-160 12-220-8v40h940z;
+                                                                                             M920 90c-50-30-120-50-200-28s-120 56-200 40-140-20-200 0-140 10-200-10v40h920z" />
+            <animate attributeName="opacity" dur="16s" repeatCount="indefinite" values="0.78;0.5;0.7;0.78" />
           </path>
         </svg>
 
